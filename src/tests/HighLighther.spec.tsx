51a06a359e68d6085a64f21fc30f3cbb6695093e
Jose Manuel Serrano Amaut <a20122128@pucp.pe>
2021-05-19 13:37:50 -0500

    [Update Test]

--- src/tests/HighLighther.spec.tsx
+++ src/tests/HighLighther.spec.tsx
@@ -37,17 +37,22 @@ it('displays a "ThemedButton" message', () => {
   expect(screen.getByText("function")).toBeInTheDocument();
   expect(screen.getAllByText("themes").length).toEqual(2);
   expect(screen.getAllByText("background").length).toEqual(3);
- //expect(screen.getByText("background")).toBeInTheDocument();
-//expect(screen.getByText("}")).toBeInTheDocument();
+  //expect(screen.getByText("background")).toBeInTheDocument();
+  //expect(screen.getByText("}")).toBeInTheDocument();
 });
-
-
 
 it("background-color must be equal to #0d1117; by default", () => {
   const { getByTestId } = render(<HighLigther code={code_test1} />);
   const HLcontainer = getByTestId("highlighter-container");
 
-  expect(HLcontainer).toHaveStyle(
-    `background-color : #0d1117`,
+  expect(HLcontainer).toHaveStyle(`background-color : #0d1117`);
+});
+
+it("background-color must be equal to #0d1117; by default", () => {
+  const { getByTestId } = render(
+    <HighLigther code={code_test1} style={{ backgroundColor: "white" }} />,
   );
+  const HLcontainer = getByTestId("highlighter-container");
+
+  expect(HLcontainer).toHaveStyle(`background-color : white`);
 });
