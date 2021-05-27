f786fb9e6d854eecc4f9159f0236471c64b5bc6f
Jose Manuel Serrano Amaut <a20122128@pucp.pe>
2021-05-27 00:30:47 -0500

    [Storybook]: Add storybook file

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 const path = require('path');
 module.exports = {
-  stories: ["../build/*.stories.tsx"],
+  stories: ["../src/*.stories.tsx"],
   addons: [
     "@storybook/addon-actions",
     "@storybook/addon-links",
